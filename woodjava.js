295471a7e6cb0922911951c2bd477c25e1828c70
gugifurniture <92727113+gugifurniture@users.noreply.github.com>
2021-12-06 12:20:10 +0900

    Update woodjava.js

--- woodjava.js
+++ woodjava.js
@@ -14,9 +14,6 @@ function timeout(obj) {
 	setTimeout("timeout(clock)", 1000); // 1초 후 timeout() 호출
 }
 
-	
-function over1(obj){obj.src="https://user-images.githubusercontent.com/92727113/144758108-d17fca76-eb2c-4257-ab97-aea355db9865.jpg"; alt="youTube"; width="30"; heighte="30"; 이메일;}
-function out1(obj){obj.src=https://user-images.githubusercontent.com/92727113/144758077-25556118-9923-4d94-b1b6-2d8203da92a6.jpg"; alt="email"; width="20"; heighte="20"; 유튜브;}
 
 	function over1(){
 	var p=document.getElementById("p2");
