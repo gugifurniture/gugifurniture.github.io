0f3ce8e5d9f17b01d6f4b8d2a60b10dbc52c872f
gugifurniture <92727113+gugifurniture@users.noreply.github.com>
2021-12-06 09:36:28 +0900

    Update woodjava.js

--- woodjava.js
+++ woodjava.js
@@ -1,6 +1,6 @@
 	function change1(){
 	var p=document.getElementById("firstP");
-	p.innerHTML="크리스마스<img src='https://user-images.githubusercontent.com/92727113/144733401-66e12c8d-bcaa-42c6-97f0-067944d0d2da.jpg'>트리짜잔";
+	p.innerHTML="크리스마스트리짜잔";
 }
 
 function startClock() {
