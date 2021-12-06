2d8f3e6609667c4434519ba8330e3fd91ad7b8f1
gugifurniture <92727113+gugifurniture@users.noreply.github.com>
2021-12-06 09:26:37 +0900

    Update woodjava.js

--- woodjava.js
+++ woodjava.js
@@ -1,6 +1,6 @@
 	function change1(){
 	var p=document.getElementById("firstP");
-	p.innerHTML="크리스마스";
+	p.innerHTML="<img src='https://user-images.githubusercontent.com/92727113/144733401-66e12c8d-bcaa-42c6-97f0-067944d0d2da.jpg'>크리스마스";
 }
 
 function startClock() {
@@ -13,3 +13,5 @@ function timeout(obj) {
 	obj.innerHTML = current.toLocaleTimeString()
 	setTimeout("timeout(clock)", 1000); // 1초 후 timeout() 호출
 }
+
+	
