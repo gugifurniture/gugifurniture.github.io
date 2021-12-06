d9e06267df655e5568e4425db8a639179d41b020
gugifurniture <92727113+gugifurniture@users.noreply.github.com>
2021-12-06 12:16:48 +0900

    Update woodjava.js

--- woodjava.js
+++ woodjava.js
@@ -17,3 +17,8 @@ function timeout(obj) {
 	
 function over1(obj){obj.src="https://user-images.githubusercontent.com/92727113/144758108-d17fca76-eb2c-4257-ab97-aea355db9865.jpg"; alt="youTube"; width="30"; heighte="30"; 이메일;}
 function out1(obj){obj.src=https://user-images.githubusercontent.com/92727113/144758077-25556118-9923-4d94-b1b6-2d8203da92a6.jpg"; alt="email"; width="20"; heighte="20"; 유튜브;}
+
+	function over1(){
+	var p=document.getElementById("p2");
+	p.innerHTML="크리스마스트리짜잔";
+}
